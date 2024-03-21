33f28d88ec972c490c13262dfe805790c05a66f5
Md Saleh <hafeezurrahman.saleh@gmail.com>
2024-03-21 03:45:00 -0500

    cicd added

--- cs544-202403-project/src/main/java/edu/miu/cs/cs544/Application.java
+++ cs544-202403-project/src/main/java/edu/miu/cs/cs544/Application.java
@@ -37,7 +37,7 @@ public class Application implements CommandLineRunner {
 //
 //    @Autowired
 //    private MemberToMemberPayloadMapper memberMapper;
-    
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
